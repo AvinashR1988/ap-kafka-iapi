93abec0b1f47ff4623f30ad1f5e10297e72f48f5
AvinashR1988 <avinashrakhundeuk@gmail.com>
2022-09-13 04:59:54 +0100

    code commit

--- src/main/java/com/cg/api/apkafkaiapi/service/ConsumerService.java
+++ src/main/java/com/cg/api/apkafkaiapi/service/ConsumerService.java
@@ -2,6 +2,7 @@ package com.cg.api.apkafkaiapi.service;
 
 import com.cg.api.apkafkaiapi.constant.AppConstants;
 import com.cg.api.apkafkaiapi.model.OrderMeta;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,15 +13,22 @@ public class ConsumerService {
 
     private final Logger logger = LoggerFactory.getLogger(ConsumerService.class);
 
-
-    @KafkaListener(topics = AppConstants.TOPIC_NAME_MESSAGE, groupId = AppConstants.GROUP_ID)
+    @KafkaListener(topics = "${general.topic.name}",
+            groupId = "${general.topic.group.id}")
     public void consume(String message) {
         logger.info(String.format("Message received -> %s", message));
     }
 
-    @KafkaListener(topics = AppConstants.TOPIC_NAME_ORDER_LOG, groupId = AppConstants.GROUP_ID)
-    public void consume(OrderMeta orderMeta) {
-        logger.info(String.format("Order received -> %s", orderMeta));
+    /*@KafkaListener(topics = "${order.topic.name}",
+            groupId = "${order.topic.group.id}",
+            containerFactory = "orderKafkaListenerContainerFactory")*/
+    @KafkaListener(topics = "${order.topic.name}",
+            groupId = "${order.topic.group.id}")
+    public void consume(ConsumerRecord<String, String> consumerRecord) {
+        System.out.println(consumerRecord);
+        logger.info(String.format("Order received -> %s", consumerRecord));
     }
+
+
 
 }
